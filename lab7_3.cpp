4f73ca97b754063a0bde00a60b23a822959127c6
NemoNJ <nemojirayu@gmail.com>
2023-12-09 14:28:45 +0700

    finish lab 7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,13 @@
-#include<iostream>
+//ไม่ต้องเติม function main()
 using namespace std;
-
 char before(char x){
-	//Write your function definition here
+if( x >= 66 && x <= 90){ //อ้างอิงตำแหน่งจาก ascii เมื่อใช้ char
+return x-1;
 }
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+else if(x == 65){
+return 'Z';
+}
+else{
+return '0';
+}
 }
